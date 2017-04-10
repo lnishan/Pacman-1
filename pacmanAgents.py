76aabaff939ce63c9bf9e583c4cadb3e369e7c74
Jasmine Chen <j.lin013@gmail.com>
2017-04-11 01:43:42 +0800

    Substitute in original files again just to be safe

--- pacmanAgents.py
+++ pacmanAgents.py
@@ -18,13 +18,8 @@ import random
 import game
 import util
 
-
 class LeftTurnAgent(game.Agent):
     "An agent that turns left at every opportunity"
-    
-    def __init__(self, evalFn="scoreEvaluation"):
-        self.evaluationFunction = util.lookup(evalFn, globals())
-        assert self.evaluationFunction != None
 
     def getAction(self, state):
         legal = state.getLegalPacmanActions()
